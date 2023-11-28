cb12d2abc8e4ed2491e307d049a724ad9c30d873
Isabella Costa <isa_bella411@hotmail.com>
2023-11-28 12:30:59 -0300

    Adicionado relacionamentos no meu banco de dados

--- AULA4.sql
+++ AULA4.sql
@@ -43,7 +43,7 @@ CREATE TABLE Turmas
 
 );
 
-CREATE TABLE Registro_presença
+CREATE TABLE Registro_Presenca
 (
 	id_turma INT NOT NULL,
 	id_aluno INT NOT NULL,
@@ -54,8 +54,32 @@ CREATE TABLE Registro_presença
 /*
 DROP TABLE Alunos 
 DROP TABLE Cursos
-DROP TABLE Registro_presença
+DROP TABLE Registro_Presenca
 DROP TABLE Situacao
 DROP TABLE Turmas
 */
+
+--Turmas x Alunos
+ALTER TABLE Turmas
+	ADD CONSTRAINT fk_TurmasAlunos FOREIGN KEY (id_aluno) REFERENCES Alunos (id_aluno);
+
+--turmas X Cursos 
+ALTER TABLE Turmas
+	ADD CONSTRAINT fk_TurmasCursos FOREIGN KEY (id_curso) REFERENCES Cursos (id_curso);
+
+--presença X Turma
+ALTER TABLE Registro_Presenca
+	ADD CONSTRAINT  fk_PresencaTurma FOREIGN KEY (id_turma) REFERENCES Turmas (id_turma);
+--presença X Aluno
+ALTER TABLE Registro_Presenca
+	ADD CONSTRAINT fk_PrensencaAluno FOREIGN KEY (id_aluno) REFERENCES Alunos (id_aluno);
+--presenca x situação
+ALTER TABLE Registro_Presenca 
+	ADD CONSTRAINT fk_presencaSituacao FOREIGN KEY(id_situacao) REFERENCES Situacao(id_situacao);
+
+
+
+
+
+
 
